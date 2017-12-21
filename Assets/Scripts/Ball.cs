5c4ba0601f463cf2583e661a3d22d2fb18174b32
Nobinator <nobinator.root@gmail.com>
2017-12-21 17:11:32 +0300

    Playable version

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour{
 
-	// Use this for initialization
+	public Vector3 startVelocity;
+	
+	Rigidbody2D rig;
+	Vector2 oldVel;
+ 
 	void Start () {
-		GetComponent<Rigidbody2D>().velocity = Vector2.down*2+Vector2.left*1;
+		rig = GetComponent<Rigidbody2D>();
+		rig.velocity = startVelocity;
+	}
+     
+	void FixedUpdate() {
+		oldVel = rig.velocity;
+	}
+
+	private void OnCollisionEnter2D (Collision2D c) {
+		var cp = c.contacts[0];
+		rig.velocity = Vector2.Reflect(oldVel,cp.normal);
+		
+		if (c.gameObject.CompareTag("Block")){
+			c.gameObject.GetComponent<Block>().Shot();
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 }
