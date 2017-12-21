80915f86c3384da0d77b43d0992c1284eafb2a06
Nobinator <nobinator.root@gmail.com>
2017-12-21 16:27:57 +0300

    Movable player and bouncable ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = Vector2.down*2+Vector2.left*1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	[SerializeField, Range(3,15)] private float startSize;
+
+	public float speed;
+
+	[SerializeField] private float moveRange;
+	
+	private float size;
+	
+	public float Size{
+		get{ return size; }
+		set{
+			size = Mathf.Clamp(value, 3, moveRange*2);
+			transform.localScale = new Vector3(size,1,1);
+		}
+	}
+
+	private void Start (){
+		Size = startSize;
+	}
+
+	private void Update(){
+		Movement();
+		Clamp();
+	}
+
+	private void Movement(){
+		float x = Input.GetAxis("Horizontal");
+		transform.Translate(Vector3.right * x * speed * Time.deltaTime);
+	}
+
+	private void Clamp(){
+		var pos = transform.position;
+		pos.x =  Mathf.Clamp(transform.position.x, -moveRange+(Size/2), moveRange-(Size/2));
+		transform.position = pos;
+	}
+
+	private void OnDrawGizmosSelected(){
+		Gizmos.DrawWireSphere(Vector3.left*moveRange, .25f);
+		Gizmos.DrawWireSphere(Vector3.right*moveRange, .25f);
+		Gizmos.DrawLine(Vector3.left*moveRange,Vector3.right*moveRange);
+	}
+
+	private void OnValidate(){
+		moveRange = moveRange < 0 ? 0 : moveRange;
+		Size = startSize;
 	}
 }
